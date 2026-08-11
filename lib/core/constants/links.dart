4039e9102992cb1f24a81288c76723b769763cb6
Ashlin <ashlin@qoruz.com>
2026-08-11 11:57:57 +0530

    feat: update WhatsApp community link

--- lib/core/constants/links.dart
+++ lib/core/constants/links.dart
@@ -1,7 +1,7 @@
 class AppLinks {
   AppLinks._();
 
-  static const whatsapp = "https://chat.whatsapp.com/IRoS5ET54VP0G7oKcNqb9T?s=sh&p=i&mlu=0&ilr=0&amv=1";
+  static const whatsapp = "https://chat.whatsapp.com/LWgNL9ldmKrFI4R6ESlikx";
   static const instagram = "https://instagram.com/tamilsocialclub";
   static const luma = "https://luma.com/ne1h96ku";
   static const website = "https://tamilsocialclub.in";
